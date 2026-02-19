108488305453bbcdb85714f59a7912eabbfbb741
SEHUZ <sonic15622@gmail.com>
2026-02-19 04:46:15 -0700

    continuacion de la practica
    correccion de layout segun orientacion, agregar themes para los layouts dark y light. Correcciones de la entrega anterior, pantalla de login con linearLayout con intent a la main activity, colores y strings guardados en variables

--- AmantePeliculas_252115/app/src/main/java/borquez/sebastian/amantepeliculas_252115/LoginActivity.kt
+++ AmantePeliculas_252115/app/src/main/java/borquez/sebastian/amantepeliculas_252115/LoginActivity.kt
@@ -1,5 +1,6 @@
 package borquez.sebastian.amantepeliculas_252115
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -7,15 +8,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val intent = Intent(this, MainActivity::class.java)
+
+        btnLogin.setOnClickListener {
+            startActivity(intent)
         }
     }
 }
